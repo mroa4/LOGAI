8e407b5201dbd72470b25d1960b32ed04ba6d0af
mroa4 <omidamini360@gmail.com>
2018-12-20 09:21:12 +0330

    del makelog

--- makeLog/main.py
+++
@@ -1,6 +0,0 @@
-from makeLog import fileReader
-import os
-
-fileReader.openFolder()
-os.chdir("../../")
-fileReader.openFolderForFeatures()
--- makeLog/object.py
+++
@@ -1,4 +0,0 @@
-class Object:
-    def __init__(self,pos,vel):
-        self.pos = pos
-        self.vel = vel
